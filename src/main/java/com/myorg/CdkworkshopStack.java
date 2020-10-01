12cef9f40bda5fa78bf126a4a9857fe407d133f8
Kudankita <41056212+Kudankita@users.noreply.github.com>
2020-10-02 01:06:56 +0900

    Hello, CDK! を完了

--- src/main/java/com/myorg/CdkworkshopStack.java
+++ src/main/java/com/myorg/CdkworkshopStack.java
@@ -1,29 +1,31 @@
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
-import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
-import software.amazon.awscdk.services.sns.Topic;
-import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
-import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.apigateway.LambdaRestApi;
+
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
 
 public class CdkworkshopStack extends Stack {
-    public CdkworkshopStack(final Construct parent, final String id) {
-        this(parent, id, null);
-    }
 
-    public CdkworkshopStack(final Construct parent, final String id, final StackProps props) {
-        super(parent, id, props);
+  public CdkworkshopStack(final Construct parent, final String id) {
+    this(parent, id, null);
+  }
 
-        final Queue queue = Queue.Builder.create(this, "CdkworkshopQueue")
-                .visibilityTimeout(Duration.seconds(300))
-                .build();
+  public CdkworkshopStack(final Construct parent, final String id, final StackProps props) {
+    super(parent, id, props);
 
-        final Topic topic = Topic.Builder.create(this, "CdkworkshopTopic")
-            .displayName("My First Topic Yeah")
-            .build();
+    final Function hello = Function.Builder.create(this, "HelloHandler")
+        .runtime(Runtime.NODEJS_10_X) // execution
+        // environment
+        .code(Code.fromAsset("lambda")) // code loaded from the "lambda" directory
+        .handler("hello.handler") // file is "hello", function is "handler"
+        .build();
 
-        topic.addSubscription(new SqsSubscription(queue));
-    }
+    // Defines an API Gateway REST API resource backed by our "hello" function
+    LambdaRestApi.Builder.create(this, "Endpoint").handler(hello).build();
+  }
 }
